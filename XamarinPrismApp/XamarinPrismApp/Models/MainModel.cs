d2228cdb089c7fdf37bf2314625e39ada705e7ab
HP-PSIA47AV\shiota <shiota@photron.co.jp>
2018-02-23 20:05:15 +0900

    Entry/Button/Label の試験
    全部 ViewModel に書いているけどね

---
+++ XamarinPrismApp/XamarinPrismApp/Models/MainModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinPrismApp.Models
+{
+    class MainModel
+    {
+        public string Title = "hogehoge";
+
+
+    }
+}
--- XamarinPrismApp/XamarinPrismApp/ViewModels/MainPageViewModel.cs
+++ XamarinPrismApp/XamarinPrismApp/ViewModels/MainPageViewModel.cs
@@ -10,10 +10,51 @@ namespace XamarinPrismApp.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+
+        // 名前入力Entry項目にバインドします
+        private string _yourName;
+        public string YourName
+        {
+            get { return _yourName; }
+            set { SetProperty(ref _yourName, value); }
+        }
+
+        // メッセージ表示Label項目にバインドします
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        // 決定ButtonのCommandにバインドします。
+        private DelegateCommand _decisionCommand;
+        public DelegateCommand DecisionCommand
+        {
+            get
+            {
+                return this._decisionCommand = this._decisionCommand ??
+                  new DelegateCommand(DecisionCommandExecute);
+            }
+        }
+
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
             Title = "Main Page";
         }
+
+        private void DecisionCommandExecute()
+        {
+            this.Message = string.Format("{0}さん こんにちは", this.YourName);
+        }
+
+        //public void OnNavigatedFrom(NavigationParameters parameters)
+        //{
+        //}
+
+        //public void OnNavigatedTo(NavigationParameters parameters)
+        //{
+        //}
     }
 }
